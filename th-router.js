85e8176fce72b082dc5142c552009db533e6b8be
jspark311 <josh.lindsay@gmail.com>
2016-03-09 13:58:55 -0700

    Initial base. Haphazrdly ripped out of MHB.

--- th-router.js
+++ th-router.js
@@ -1,7 +1,6 @@
 'use strict'
 
 var inherits = require('util').inherits;
-var ee = require('events').EventEmitter;
 var fs = require("fs");
 var th = require("telehash");
 
@@ -28,10 +27,7 @@ require('dns').lookup(require('os').hostname(), function (err, add, fam) {
 var loadKeyData = function(caller, _path, callback) {
   if (fs.existsSync(_path) && fs.lstatSync(_path).isFile()) {
     var return_value = JSON.parse(fs.readFileSync(_path));
-    caller.send('log', {
-      body: 'Loaded endpoint ID: ' + return_value.hashname,
-      verbosity: 5
-    });
+    console.log('Loaded endpoint ID: ' + return_value.hashname);
     if (callback) {
       callback(false, return_value);
     }
@@ -40,10 +36,7 @@ var loadKeyData = function(caller, _path, callback) {
     th.generate(
       function(err, endpoint) {
         if (err) {
-          caller.send('log', {
-            body: 'Error generating endpoint ID: ' + err,
-            verbosity: 2
-          });
+          console.log('Error generating endpoint ID: ' + err);
           if (callback) {
             callback(err, false);
           }
@@ -52,16 +45,10 @@ var loadKeyData = function(caller, _path, callback) {
           fs.writeFile(_path, JSON.stringify(endpoint),
             function(err1) {
               if (err1) {
-                caller.send('log', {
-                  body: 'Generated router ID but failed to retain it in '+_path+'. ' + err1,
-                  verbosity: 3
-                });
+                console.log('Generated router ID but failed to retain it in '+_path+'. ' + err1);
               }
               else {
-                caller.send('log', {
-                  body: 'Generated and stored endpoint ID: ' + endpoint.hashname,
-                  verbosity: 6
-                });
+                console.log('Generated and stored endpoint ID: ' + endpoint.hashname);
               }
             }
           );
@@ -77,8 +64,6 @@ var loadKeyData = function(caller, _path, callback) {
 
 // EXPOSED OBJECT / CONSTRUCTOR
 function mTransport(options) {
-  ee.call(this);
-
   // set scope for private methods
   var that = this;
   var connect_state = false;
@@ -130,14 +115,12 @@ function mTransport(options) {
 
 
 function mTransportFactory() {
-  ee.call(this);
-
   // set scope for private methods
   var that = this;
 
   // TODO: All files that contain identity information should be encrypted.
-  this.router_id_path = DEFAULT_ID_PATH;  // Path to the default router ID file.
-  this.router_mesh    = null;
+  var router_id_path  = DEFAULT_ID_PATH;  // Path to the default router ID file.
+  var router_mesh     = null;
   var router_id       = null;
   var discovery_state = false;
 
@@ -153,10 +136,10 @@ function mTransportFactory() {
       that.send('localAddress', '');
     }
     else {
-      that.router_mesh = _mesh;
+      router_mesh = _mesh;
       console.log('Mesh started. Router ID is ' + router_id.hashname);
       that.send('listening', true);
-      that.send('localAddress', that.router_mesh.uri());
+      that.send('localAddress', router_mesh.uri());
     }
   }
 
@@ -175,17 +158,17 @@ function mTransportFactory() {
   this.discover = function(_discoverable, _discvr_timeout) {
     if (discovery_state ^ _discoverable) {
       if (_discoverable) {
-        that.router_mesh.accept = function(from) {
+        router_mesh.accept = function(from) {
           console.log('TelehashFactory is accepting a link from: ' + JSON.stringify(from));
-          var _link = that.router_mesh.link(from);
+          var _link = router_mesh.link(from);
           me.send('connected', new mTransport({link: _link}));
         }
       }
       else {
-        that.router_mesh.accept = undefined;
+        router_mesh.accept = undefined;
       }
 
-      that.router_mesh.discover(_discoverable,
+      router_mesh.discover(_discoverable,
         function() {
           that.discoveryState(_discoverable);
         }
@@ -220,24 +203,23 @@ function mTransportFactory() {
   }
 
 
-  this.listen = function(me, data) {
+  this.listen = function(d_state, _keyfile) {
     if (data.length === 0) {
       console.log('Insufficient parameter count.');
       return;
     }
-    var d_state  = data.shift();   // The desired listening state.
     // The file containing the desired identity.
-    var _keyfile = (data.length > 0) ? data.shift() : me.router_id_path;
-    if (me.router_mesh && !d_state) {
+    _keyfile = (_keyfile) ? _keyfile : router_id_path;
+    if (router_mesh && !d_state) {
       // Router is running and ought not be.
-      me.router_mesh.close();
+      router_mesh.close();
       me.send('listening', false);
       me.send('localAddress', '');
-      me.router_mesh = null;
+      router_mesh = null;
     }
     else if (d_state) {
       // Start the server listening.
-      if (!me.router_mesh) {
+      if (!router_mesh) {
         loadKeyData(me, _keyfile, me.startRouter);
       }
       else {
@@ -251,13 +233,11 @@ function mTransportFactory() {
 
 
 
-  this.discovery = function(data) {
-    if (me.router_mesh) {
-      var _discoverable   = false;
-      var _discvr_timeout = false;
-      if (data.length > 0) _discoverable   = (data.shift()) ? true : false;
-      if (data.length > 0) _discvr_timeout = data.shift();
-      me.discover(_discoverable, _discvr_timeout);
+  this.discovery = function(_discoverable, _discvr_timeout) {
+    if (router_mesh) {
+      _discoverable   = (_discoverable)   ? true : false;
+      _discvr_timeout = (_discvr_timeout) ? _discvr_timeout : 0;
+      discover(_discoverable, _discvr_timeout);
     }
     else {
       console.log('TelehashFactory is not yet running a mesh.');
@@ -265,13 +245,12 @@ function mTransportFactory() {
   }
 
 
-  this.connect = function(me, data) {
-    if (data.length > 0) {
-      var _hashname  = data.shift();
-      var _keyfile = (data.length > 0) ? data.shift() : DEFAULT_CLIENT_ID_PATH;
-      if (me.router_mesh) {
+  this.connect = function(_hashname, _keyfile) {
+    if (_hashname) {
+      _keyfile = (_keyfile) ? _keyfile : DEFAULT_CLIENT_ID_PATH;
+      if (router_mesh) {
         console.log('Attempting connection to ' + _hashname + " with keyfile " + _keyfile)
-        var nu_link = me.router_mesh.link(_hashname);
+        var nu_link = router_mesh.link(_hashname);
         me.send('connected', new mTransport({link: nu_link}));
       }
       else {
