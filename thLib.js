d9606438d6c0cdbf059faf665eddfb2afabaa954
Zone-Ghost <josh.treadwell@gmail.com>
2016-03-11 11:55:55 -0800

    no-console turned off

--- thLib.js
+++ thLib.js
@@ -1,27 +1,27 @@
 'use strict'
 
-const fs = require("fs");
-const th = require("telehash");
+const fs = require('fs');
+const th = require('telehash');
 const inpsect = require('util').inspect;
 const EventEmitter = require('events');
 
 class Emitter extends EventEmitter {}
 
-let defReg = {
-    "keys": {
-        "1a": "akhb6r3agdzxq35jxp2s2bp4ll6j46jjcu",
-        "3a": "yn33zmwe6qw4hzi56evjfitjr6wslkttjozcziu3g66rpdgdwvxq",
-        "2a": "gcbacirqbudaskugjcdpodibaeaqkaadqiaq6abqqiaquaucaeaqbingh2rf7xnrgffxlfpfal2n3dfe2mfvucl7gyche7bczrxvox4cwg7wepdrdu66ceypnkpchimsgi5r555dwnge3xvcl7753loidkus64ospm2lph35okxudbtwxqwzgolrldbbxsdu44nzr3m5amjtaufhkdsanokjdlrbw35ozafhxrtts2o6pw56f6jk3hyjvkktsla6vijwo42bzznbddnk7iwzqxfl76anbc7fmnq5fdl3rsqgb5m2nwv66lbsgenuvp3sdf6jth3jgfnmjtlef4ykpd4la3oyx2zfowwoie2u4iefhqdjv7okmg62kz22yoo35ffwjmfenkcmdfvjrt6aannr4ll2bw4yjarlktrx77bzarftnwl3fkmyqq3kc425bd6n7z2cqouyz7ycamaqaai"
+const defReg = {
+    keys: {
+        '1a' : 'akhb6r3agdzxq35jxp2s2bp4ll6j46jjcu',
+        '3a' : 'yn33zmwe6qw4hzi56evjfitjr6wslkttjozcziu3g66rpdgdwvxq',
+        '2a' : 'gcbacirqbudaskugjcdpodibaeaqkaadqiaq6abqqiaquaucaeaqbingh2rf7xnrgffxlfpfal2n3dfe2mfvucl7gyche7bczrxvox4cwg7wepdrdu66ceypnkpchimsgi5r555dwnge3xvcl7753loidkus64ospm2lph35okxudbtwxqwzgolrldbbxsdu44nzr3m5amjtaufhkdsanokjdlrbw35ozafhxrtts2o6pw56f6jk3hyjvkktsla6vijwo42bzznbddnk7iwzqxfl76anbc7fmnq5fdl3rsqgb5m2nwv66lbsgenuvp3sdf6jth3jgfnmjtlef4ykpd4la3oyx2zfowwoie2u4iefhqdjv7okmg62kz22yoo35ffwjmfenkcmdfvjrt6aannr4ll2bw4yjarlktrx77bzarftnwl3fkmyqq3kc425bd6n7z2cqouyz7ycamaqaai'
     },
-    "hashname": "5kl4lesg6uouxopelrbjubk5yeouo3e3dtumm5rl6vamdwxqd2eq"
+    'hashname': '5kl4lesg6uouxopelrbjubk5yeouo3e3dtumm5rl6vamdwxqd2eq'
 }
 let defRouter = {
-    "keys": {
-        "1a": "akkjwabmjlacnr57nmy7kufyk34frcqzxy",
-        "3a": "dmthbylhjijqjrb4ufgz5dfrqutemabun5ebaswe3jqg4v7yorma",
-        "2a": "gcbacirqbudaskugjcdpodibaeaqkaadqiaq6abqqiaquaucaeaqbjg3ulby4sbak55lirwqme2yxyx4fuuucmxnf6ajmspascibnv3zt2awpnuw3vztti6k2vbh7pc63jh7bqltn25n3wbznrk3on7i3payxdjf33rnggllrpk77jibkgr2zyam2nqjaq7ldgapqgh6vcjz3nrfhsr55qznhmphumrd73otk5rufappeod2mnbe3bgq3avpudad6utbe45hsq2ip6yfqvqp2a6tu33fd7zzety44awyb5cvl2txm4ff7chun27fyl5r24cb25rup7snrg677r2v5q3wm5woeg4jtnfmkv7c2l2kakojlrgadpplsh7xiyifkgdsikiw526kxap73rlzp4wee6hx6vucawe2gciw7vbweqz6yygdelp3z7pwr7zf5yqnqhl66wgwbfycamaqaai"
-    },
-    "hashname": "kw4qurandncxwokuidfx6nkewvqivqwbab6fzsv4q7t5gjupyc2a"
+  'keys': {
+    '1a': 'akkjwabmjlacnr57nmy7kufyk34frcqzxy',
+    '3a': 'dmthbylhjijqjrb4ufgz5dfrqutemabun5ebaswe3jqg4v7yorma',
+    '2a': 'gcbacirqbudaskugjcdpodibaeaqkaadqiaq6abqqiaquaucaeaqbjg3ulby4sbak55lirwqme2yxyx4fuuucmxnf6ajmspascibnv3zt2awpnuw3vztti6k2vbh7pc63jh7bqltn25n3wbznrk3on7i3payxdjf33rnggllrpk77jibkgr2zyam2nqjaq7ldgapqgh6vcjz3nrfhsr55qznhmphumrd73otk5rufappeod2mnbe3bgq3avpudad6utbe45hsq2ip6yfqvqp2a6tu33fd7zzety44awyb5cvl2txm4ff7chun27fyl5r24cb25rup7snrg677r2v5q3wm5woeg4jtnfmkv7c2l2kakojlrgadpplsh7xiyifkgdsikiw526kxap73rlzp4wee6hx6vucawe2gciw7vbweqz6yygdelp3z7pwr7zf5yqnqhl66wgwbfycamaqaai'
+  },
+  'hashname': 'kw4qurandncxwokuidfx6nkewvqivqwbab6fzsv4q7t5gjupyc2a'
 }
 
 function telehashMesh(_config, callback){
@@ -41,16 +41,18 @@ function telehashMesh(_config, callback){
   // Option merging and optional generation
   if(!callback) { callback = () => {} }
 
+  console.log('derp');
+
   const build = () => {
 
     mesh = th.mesh({ id: config.endpoint_id }, (err, cb) => {
       if(err) {
-        callback(new Error("Couldn't generate mesh:" + err))
+        callback(new Error(`Couldn\'t generate mesh: ${err}`))
         return
       }
 
-      linkRefs["router"] = mesh.link(config.router_id);
-      linkRefs["registry"] = mesh.link(config.registry_id);
+      linkRefs['router'] = mesh.link(config.router_id);
+      linkRefs['registry'] = mesh.link(config.registry_id);
       config.authorized_ids.forEach((v) => {
         linkRefs[v.hashname] = this.link(v);
       })
@@ -118,11 +120,11 @@ function telehashMesh(_config, callback){
   }
 
   // construction
-  if(typeof _config === "object") Object.assign(config, _config);
+  if(typeof _config === 'object') Object.assign(config, _config);
   if(config.endpoint_id === null) {
     th.generate((err, endpoint) => {
       if(err) {
-        callback(new Error("Couldn't generate endpoint"))
+        callback(new Error('Couldn\'t generate endpoint'))
         return;
       }
       config.endpoint_id = endpoint;
@@ -140,7 +142,7 @@ module.exports = {
     if (fs.existsSync(_path) && fs.lstatSync(_path).isFile()) {
       _callback(false, JSON.parse( fs.readFileSync(_path) ));
     } else {
-      _callback(new Error("Failed to load provided file"));
+      _callback(new Error('Failed to load provided file'));
     }
   },
 }
